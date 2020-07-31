3cc66a3e65199819612c9d6b88777c8ec841118e
LanxinZhang <lxzhang1995@gmail.com>
2020-07-30 20:13:08 -0700

    Add SB function

--- src/Hello.java
+++ src/Hello.java
@@ -6,5 +6,10 @@ public class Hello {
         para1 += para2;
     }
 
+    public static void StringAdd(String param1) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(param1);
+    }
+
 }
 
